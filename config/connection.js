1eb38ff4ab369e630d837a39674f7a851ceb584a
JB <jmb2883@gmail.com>
2018-06-27 22:23:19 -0400

    updating connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST || "localhost",
   port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db"
+  user: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "rootroot",
+  database: process.env.DB_DATABASE || "burgers_db"
 });
-
+mysql://:04e2d7b1@us-cdbr-iron-east-04.cleardb.net/heroku_e4981d1b3a22396?reconnect=true
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
