926de38fd352fb963a02c69967496fe99d30267e
JB <jmb2883@gmail.com>
2018-06-27 22:24:27 -0400

    removing info

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   password: process.env.DB_PASSWORD || "rootroot",
   database: process.env.DB_DATABASE || "burgers_db"
 });
-mysql://:04e2d7b1@us-cdbr-iron-east-04.cleardb.net/heroku_e4981d1b3a22396?reconnect=true
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
